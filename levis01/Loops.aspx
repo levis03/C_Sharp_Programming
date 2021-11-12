6ee1f261ee7707d488b301b0c29b6931600aa376
STUDENT <STUDENT@DESKTOP-SCIK7VA>
2021-11-12 01:24:13 -0800

    Add project files.

---
+++ levis01/Loops.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Levis.Master" AutoEventWireup="true" CodeBehind="Loops.aspx.cs" Inherits="levis01.Loops" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <asp:Button ID="btnDowhile" runat="server" OnClick="btnDowhile_Click" Text="Do While Loop" />
+    <br />
+    <asp:Button ID="btnWhile" runat="server" OnClick="Button2_Click" Text="While Loop" />
+    <br />
+    <asp:Button ID="btnForloop" runat="server" OnClick="btnForloop_Click" Text="for Loop" />
+    <br />
+    <asp:Button ID="btnforeach" runat="server" OnClick="btnforeach_Click" Text="foreach Loop" />
+</asp:Content>
---
+++ levis01/Loops.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace levis01
+{
+    public partial class Loops : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Declare global variable
+        int k; //k shall be the loop variable
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            k = 100;
+
+            {
+                Response.Write("Working with the while loop " + "<br>");
+                while (k > 0)
+                {
+                    if (k % 2 == 1)
+                    {
+                        Response.Write(k + "<br>");
+                    }
+                }
+                k--;
+            }
+        }
+
+        protected void btnDowhile_Click(object sender, EventArgs e)
+        {
+            k = 100;
+            do
+            {
+                Response.Write("Working with do while " + "<br>");
+                {
+                    if (k % 2 == 1)
+                        Response.Write(k + "<br>");
+
+                }
+                k--;
+            } while (k > 0);
+        }
+
+        protected void btnForloop_Click(object sender, EventArgs e)
+        {
+            Response.Write("For loop <br>");
+            for (k = 100; k > 0; k--)
+            {
+                if (k % 2 == 1)
+                {
+                    Response.Write(k + "<br>");
+                }
+            }
+        }
+
+        protected void btnforeach_Click(object sender, EventArgs e)
+        {
+            int[] age = { 4, 8, 10, 45, 30, 12, 67, 24, 21, 89 };
+            int total = 0;
+            //start foreach loop
+            foreach (int m in age)
+            {
+                if (m > 18)
+                {
+                    Response.Write(m + "<br>");
+                    total++;
+
+                }
+            }
+            Response.Write("Total number of people above 18:\t" + total);
+
+        }
+    }
+}
